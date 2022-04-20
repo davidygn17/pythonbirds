54832577943d0fad686452871f58074f2705f6b6
David Nunes <98975085+davidygn17@users.noreply.github.com>
2022-04-20 22:14:07 +0100

    criado atributo de class

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,4 +1,5 @@
 class Pessoa:
+    olhos = 2
     def __init__(self, *filhos,  nome=None, idade=29 ):
         self.idade = idade
         self.nome = nome
@@ -20,5 +21,12 @@ if __name__ == '__main__':
         print(filho.nome)
     luciano.sobrenome = 'Ramalho'
     del luciano.filhos
+    luciano.olhos = 1
+    del luciano.olhos
     print(luciano.__dict__)
     print(david.__dict__)
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)
+    print(luciano.olhos)
+    print(david.olhos)
+    print(id(Pessoa.olhos), id(luciano.olhos), id(david.olhos))
